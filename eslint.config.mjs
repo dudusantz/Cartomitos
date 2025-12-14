2b4a88debe5e931d1b7a88d353281187c468d27c
dudusantz <eduardoviniciusx05@gmail.com>
2025-12-13 21:56:35 -0300

    Desativando regras estritas do ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,15 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // --- ADICIONE ESTE BLOCO PARA DESATIVAR AS REGRAS ESTRITAS ---
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off",
+    }
+  },
+  // -------------------------------------------------------------
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
